a713efc93867f3a3da475542522802ebac9d2e81
ogustavxs <gustavopessoasillva@gmail.com>
2026-08-16 01:27:40 -0300

    feat: estilo e icones da Topbar atualizados

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Sun, CircleQuestionMark } from "lucide-react";
 
 export default function Topbar() {
   return (
-    <header className="flex justify-between h-12 items-center px-5 border-b border-b-slate-700">
-        <h6 className="text-cyan-200 text-xs font-medium tracking-widest">TOTVS IFLOW - PAINEL</h6>
+    <header className="flex justify-between h-[3.8rem] items-center px-5 border-b border-b-slate-700">
+        <h6 className="text-accent-500 text-xs font-bold tracking-widest">TOTVS IFLOW</h6>
         <aside className="flex gap-4">
-            <div className="flex gap-2 px-3 border-r border-r-slate-600 items-center font-semibold text-slate-400 ">
-                <p>☀️</p>
-                <p className="bg-slate-700 rounded-full flex items-center justify-center w-6 h-6">?</p>
+            <div className="flex gap-5 px-5 border-r border-r-slate-800 items-center font-semibold text-slate-500 ">
+                <Sun size={18} className="cursor-pointer hover:text-white" />
+                <CircleQuestionMark size={18} className="cursor-pointer hover:text-accent-500" />
             </div>
-            <p className="bg-cyan-500 text-xs rounded-full flex items-center justify-center w-7 h-7 text-white font-bold">RR</p>
+            <p className="bg-accent-500 select-none rounded-full flex items-center justify-center w-8 h-8 text-sm text-white font-bold">RR</p>
         </aside>
     </header>
   )
