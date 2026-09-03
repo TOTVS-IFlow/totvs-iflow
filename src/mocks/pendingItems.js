45d9d1df901a1e4f66903f43ac4239a8de4be83a
ogustavxs <gustavopessoasillva@gmail.com>
2026-09-03 15:16:12 -0300

    refactor: separa os padroes e dados mockados

---
+++ src/mocks/pendingItems.js
@@ -0,0 +1,18 @@
+export const PENDING_ITEMS = [
+  {
+    id: 1,
+    description: "Agendar 2ª fase com especialista de escalas e sindicatos",
+    owner: "Eng. de vendas TOTVS",
+    status: "open",
+    meetingId: 1,
+    client: "Grupo Vetor Logística",
+  },
+  {
+    id: 2,
+    description: "Validar parametrização das 32 convenções coletivas",
+    owner: "Especialista TOTVS",
+    status: "done",
+    meetingId: 1,
+    client: "Grupo Vetor Logística",
+  },
+];
